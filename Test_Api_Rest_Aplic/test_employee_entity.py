1a98a3317070e74a8a80fa91cc1daad9b910e096
Uriy Maslov <umaslov@netlabsystems.com>
2019-11-11 21:01:48 +0500

    Added DELETE and GET Methods for Message minor fix some tests

--- Test_Api_Rest_Aplic/test_employee_entity.py
+++ Test_Api_Rest_Aplic/test_employee_entity.py
@@ -11,8 +11,8 @@ DATA = {"name": "Test"}
 
 
 @pytest.mark.parametrize('endpoint3', [ENDPOINT3])
-@pytest.mark.parametrize('param1', PARAM1)
-def test_endpoints_encoding(client, endpoint3, param1):
+@pytest.mark.parametrize('param1', [PARAM1])
+def test_endpoints_employee(client, endpoint3, param1):
     """Check employee in list"""
     endpoint = '/'.join([endpoint3, param1])
     response = client.do_get(endpoint)
@@ -20,9 +20,9 @@ def test_endpoints_encoding(client, endpoint3, param1):
     assert jsons[0]['text'] == 'Maslov'
 
 
-@pytest.mark.parametrize('endpoint5', ENDPOINT5)
-@pytest.mark.parametrize('param2', PARAM2)
-def test_endpoints_encoding(client, endpoint5, param2):
+@pytest.mark.parametrize('endpoint5', [ENDPOINT5])
+@pytest.mark.parametrize('param2', [PARAM2])
+def test_endpoints_by_division(client, endpoint5, param2):
     """Find employee dy division"""
     endpoint = '?'.join([endpoint5, param2])
     response = client.do_get(endpoint)
@@ -30,9 +30,9 @@ def test_endpoints_encoding(client, endpoint5, param2):
     assert jsons[0]['text'] == 'Maslov'
 
 
-@pytest.mark.parametrize('endpoint3', ENDPOINT3)
-@pytest.mark.parametrize('param3', PARAM3)
-def test_endpoints_encoding(client, endpoint3, param3):
+@pytest.mark.parametrize('endpoint3', [ENDPOINT3])
+@pytest.mark.parametrize('param3', [PARAM3])
+def test_endpoints_by_name(client, endpoint3, param3):
     """Find employee dy name"""
     endpoint = '?'.join([endpoint3, param3])
     response = client.do_get(endpoint)
@@ -40,51 +40,51 @@ def test_endpoints_encoding(client, endpoint3, param3):
     assert jsons[0]['text'] == 'Maslov'
 
 
-@pytest.mark.parametrize('endpoint4', ENDPOINT4)
-def test_endpoints_encoding(client, endpoint4):
+@pytest.mark.parametrize('endpoint4', [ENDPOINT4])
+def test_endpoints_all_employees(client, endpoint4):
     """Find all employees"""
     response = client.do_get(endpoint4)
     jsons = response.json()
     assert jsons[0]['text'] == 'Maslov'
 
 
-@pytest.mark.parametrize('endpoint', ENDPOINTS)
+@pytest.mark.parametrize('endpoint', [ENDPOINTS])
 def test_status_code(client, endpoint):
     """Status Code"""
     response = client.do_get(endpoint)
     assert response.status_code == 200
 
 
-@pytest.mark.parametrize('endpoint', ENDPOINTS)
+@pytest.mark.parametrize('endpoint', [ENDPOINTS])
 def test_headers_content_type(client, endpoint):
     """Content type"""
     response = client.do_get(endpoint)
     assert response.headers['Content-type'] == 'application/json'
 
 
-@pytest.mark.parametrize('endpoint', ENDPOINTS)
+@pytest.mark.parametrize('endpoint', [ENDPOINTS])
 def test_cookies(client, endpoint):
     """Check Cookies"""
     response = client.do_get(endpoint)
     assert response.cookies
 
 
-@pytest.mark.parametrize('endpoint', ENDPOINTS)
+@pytest.mark.parametrize('endpoint', [ENDPOINTS])
 def test_url(client, endpoint):
     """Check Url"""
     response = client.do_get(endpoint)
     assert response.url
 
 
-@pytest.mark.parametrize('endpoints', ENDPOINTS)
+@pytest.mark.parametrize('endpoints', [ENDPOINTS])
 def test_endpoint_json(client, endpoints):
     """Json"""
     j_s = client.do_json(endpoints)
     assert j_s['status'] == 'success'
 
 
-@pytest.mark.parametrize('endpoint', ENDPOINTS)
-@pytest.mark.parametrize('data', DATA)
+@pytest.mark.parametrize('endpoint', [ENDPOINTS])
+@pytest.mark.parametrize('data', [DATA])
 def test_endpoints_post(client, endpoint, data):
     """POST Status Code"""
     response = client.do_post(endpoint, data)
--- Test_Api_Rest_Aplic/test_message_entity.py
+++ Test_Api_Rest_Aplic/test_message_entity.py
@@ -1,11 +1,23 @@
 import pytest
 
 ENDPOINT1 = 'message'
+PARAM = '1'
+PARAM2 = '5'
 
 
 @pytest.mark.parametrize('endpoint1', [ENDPOINT1])
-def test_endpoints_encoding(client, endpoint1):
-    """Encoding"""
+def test_endpoints_get_text(client, endpoint1):
+    """Get text"""
     response = client.do_get(endpoint1)
     jsons = response.json()
-    assert jsons[0]['text'] == 'First message'
+    assert jsons[0]['text'] == 'Optional'
+
+
+@pytest.mark.parametrize('param', [PARAM])
+@pytest.mark.parametrize('endpoint1', [ENDPOINT1])
+def test_endpoint_delete(client, endpoint1, param):
+    endpoint = '/'.join([endpoint1, param])
+    response = client.do_delete(endpoint)
+    resp = client.do_get(endpoint)
+    assert resp.text == 'null'
+    assert response.status_code == 500
